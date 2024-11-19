9082d5e02886dd87b165eb8a8edbcc990817a2f7
Mauricio <mauba22@outlook.com>
2024-11-19 11:51:36 -0600

    Se corrigió error de la propia api en URL del módulo EPIC

--- src/app/pages/epic/service/epic.service.ts
+++ src/app/pages/epic/service/epic.service.ts
@@ -13,7 +13,7 @@ export class EpicService {
 
   /* Obtiene la lista de imágenes del EPIC */
   getListByNaturalQuery(): Observable<Epic[]> {
-    return this.http.get<Epic[]>(`${environment.url}EPIC/api/natural?api_key=${environment.key}`)
+    return this.http.get<Epic[]>(`${environment.epicUrl}api/natural `)
       .pipe(
         map((res: Epic[]) => {
           return res
